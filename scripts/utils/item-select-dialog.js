5fb001ffacee4546fb72b85eeb5e56f796812a0b
Jonathan Calvert <jd.calvert@hotmail.co.uk>
2022-03-30 22:21:21 +0100

    Fix formatting

--- scripts/utils/item-select-dialog.js
+++ scripts/utils/item-select-dialog.js
@@ -15,34 +15,34 @@ export class ItemSelectDialog extends Dialog {
                 width: "100%",
                 id: "item-dialog"
             }
-        )
+        );
     }
 
     static async getItem(title, header, items) {
         let content = `
             <div class="item-buttons" style="max-width: max-content; justify-items: center; margin: auto;">
             <p>${header}</p>
-        `
+        `;
 
         for (const itemCategory of items.keys()) {
             content += `
                 <fieldset style="border: 1px solid #a1a1a1; padding: 5px;">
                     <legend>${itemCategory}</legend>
-            `
-            
+            `;
+
             for (let item of items.get(itemCategory)) {
                 content += `
                     <button class="item-button" type="button" value="${item.id}" style="display: flex; align-items: center; margin: 4px auto">
                         <img src="${item.img}" style="border: 1px solid #444; height: 1.6em; margin-right: 0.5em"/>
                         <span>${item.name}</span>
                     </button>
-                `
+                `;
             }
 
             content += `</fieldset>`;
         }
 
-        content += `</div>`
+        content += `</div>`;
         let itemId = await new this(title, content).getItemId();
         return Array.from(items.values()).flat().find(item => item.id === itemId);
     }
