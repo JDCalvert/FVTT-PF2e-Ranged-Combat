ff6a4f5c6081b93999c7ab3088ec5ea6e8c141c6
Jonathan Calvert <jd.calvert@hotmail.co.uk>
2022-07-15 12:03:02 +0100

    small fixes

--- scripts/utils/migrations/migration-002-thrown-weapon-groups.js
+++ scripts/utils/migrations/migration-002-thrown-weapon-groups.js
@@ -1,12 +1,11 @@
 import { useAdvancedThrownWeaponSystem } from "../../thrown-weapons/utils.js";
 import { getFlags } from "../utils.js";
-import { getWeapons } from "../weapon-utils.js";
 
 export class Migration002ThrownWeaponGroups {
     version = 2;
 
     async runMigration() {
-        console.info("PF2e Ranged Combat - Running Migration 1: Multiple Ammunition Update");
+        console.info("PF2e Ranged Combat - Running Migration 2: Thrown Weapon Groups");
 
         const actors = game.actors.contents;
 
@@ -20,58 +19,52 @@ export class Migration002ThrownWeaponGroups {
             const weapons = actor.itemTypes.weapon;
 
             for (const weapon of weapons) {
-                weapon.unsetFlag("pf2e-ranged-combat", "groupIds");
-
-                const flags = getFlags(weapon)?.droppedFrom;
-                if (!flags) {
+                const droppedFrom = getFlags(weapon)?.droppedFrom;
+                if (!droppedFrom) {
                     continue;
                 }
 
-                const droppedFrom = flags.droppedFrom;
-                if (droppedFrom) {
+                // Remove the droppedFrom flag and reset the name
+                updates.push(
+                    {
+                        _id: weapon.id,
+                        name: droppedFrom.name,
+                        flags: {
+                            "pf2e-ranged-combat": {
+                                "-=droppedFrom": null
+                            }
+                        }
+                    }
+                );
 
-                    // Remove the droppedFrom flag and reset the name
+                // Look for the weapon this was dropped from
+                const originalWeapon = weapons.find(w => w.id === droppedFrom.id);
+                if (originalWeapon) {
+                    const groupIds = [originalWeapon.id, weapon.id];
+
+                    // Update the original weapon to set the group
                     updates.push(
                         {
-                            _id: weapon.id,
-                            name: droppedFrom.name,
+                            _id: originalWeapon.id,
                             flags: {
                                 "pf2e-ranged-combat": {
-                                    "-=droppedFrom": null
+                                    groupIds
                                 }
                             }
                         }
                     );
 
-                    // Look for the weapon this was dropped from
-                    const originalWeapon = weapons.find(w => w.id === droppedFrom.id);
-                    if (originalWeapon) {
-                        const groupIds = [originalWeapon.id, weapon.id];
-
-                        // Update the original weapon to set the group
-                        updates.push(
-                            {
-                                _id: originalWeapon.id,
-                                flags: {
-                                    "pf2e-ranged-combat": {
-                                        groupIds
-                                    }
+                    // Update the current weapon to set the group
+                    updates.push(
+                        {
+                            _id: weapon.id,
+                            flags: {
+                                "pf2e-ranged-combat": {
+                                    groupIds
                                 }
                             }
-                        );
-
-                        // Update the current weapon to set the group
-                        updates.push(
-                            {
-                                _id: weapon.id,
-                                flags: {
-                                    "pf2e-ranged-combat": {
-                                        groupIds
-                                    }
-                                }
-                            }
-                        );
-                    }
+                        }
+                    );
                 }
             }
 
